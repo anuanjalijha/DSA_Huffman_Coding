8310f1c8de78b17ae75e86ea628db238beb46a8c
Anjali <anjalijanar@gmail.com>
2023-11-14 22:07:27 +0530

    Basic Implementation of Huffman Coding

---
+++ ImplementationOfHuffmanCoding.py
@@ -0,0 +1,23 @@
+class HuffmanCoding:
+    def __init__(self,path):
+        self.path=path
+        
+    def __make_frequency_dict(self):
+        #get file from path
+        
+        #read text from file
+        
+        #make frequency dictionary using the text
+        
+        #construct the heap from the frequency_dict
+        
+        #construct the binary tree from the heap
+        
+        #creating the encoded text using the codes
+        
+        #put this encoded text into the binary file
+        
+        #return the binary file as output
+        
+        pass
+        
